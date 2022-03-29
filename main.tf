9008c2a922e6cb44605e2dbb45bc4439b7b9d7a0
Ryan Pecor <rpecor14@gmail.com>
2022-03-28 19:05:29 -0500

    fix

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "s_vnet_rg" {
 resource "azurerm_virtual_network" "s_vnet" {
   name = var.v_name
   location = var.location
-  resource_group_name = azurerm_resource_group.s_vnet.name
+  resource_group_name = azurerm_resource_group.s_vnet_rg.name
   address_space = [var.v_cidr]
 
   tags = var.tags
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network" "s_vnet" {
 resource "azurerm_network_security_group" "s_nsg" {
   name = var.nsg_name
   location = var.location
-  resource_group_name = azurerm_resource_group.s_vnet.name
+  resource_group_name = azurerm_resource_group.s_vnet_rg.name
 
   tags = var.tags
 }
