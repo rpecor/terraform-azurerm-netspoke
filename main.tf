e9f180710b6cbe1498a21424ae67b99e920715ea
Ryan Pecor <rpecor14@gmail.com>
2022-03-28 22:27:07 -0500

    add name...

--- main.tf
+++ main.tf
@@ -29,6 +29,6 @@ resource "azurerm_subnet" "spoke_subnet" {
   }
   name = "${each.value.subnet_name}"
   address_prefixes = [each.value.address_prefix]
-  resource_group_name = azurerm_resource_group.s_vnet_rg
-  virtual_network_name = azurerm_virtual_network.s_vnet
+  resource_group_name = azurerm_resource_group.s_vnet_rg.name
+  virtual_network_name = azurerm_virtual_network.s_vnet.name
 }
